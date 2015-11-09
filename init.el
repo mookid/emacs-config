7e1938a0225fcae0c735a8e1cdeabdea00e6942b
mookid <nathan.moreau@m4x.org>
2015-11-09 19:37:30 +0100

    Add helm settings

--- init.el
+++ init.el
@@ -129,3 +129,10 @@
  "Loading paredit mode"
  (require 'paredit)
  (paredit-mode))
+
+(with-message
+ "Loading helm"
+ (require 'helm)
+ (global-set-key (kbd "M-x") 'helm-M-x)
+ (global-set-key (kbd "C-x C-m") 'helm-M-x)
+ (global-set-key (kbd "C-x C-b") 'helm-buffers-list))
