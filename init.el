a1e1485e21080d087754efabb2a2bf81d5beecc3
mookid <nathan.moreau@m4x.org>
2015-11-21 22:28:25 +0100

    Cleanup.

--- init.el
+++ init.el
@@ -346,17 +346,3 @@ See `toggle-selective-display' and `increase-selective-display'."
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (shackle ocp-indent tuareg smartparens smart-mode-line-powerline-theme rainbow-delimiters rainbow-blocks paredit flycheck evil-visualstar evil-search-highlight-persist evil-numbers evil-matchit evil-magit company ace-isearch))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
