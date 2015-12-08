e38a8fc5c539665e11defaa8374e090635176f1a
mookid <nathan.moreau@m4x.org>
2015-12-09 00:28:20 +0100

    Cosmetic.

--- lisp/mookid-c.el
+++ lisp/mookid-c.el
@@ -6,8 +6,8 @@
 (defvar c-mode-base-map)
 (add-hook 'c-initialization-hook
           (lambda ()
-               (setq-default c-default-style "linux" c-basic-offset 8)
-               (define-key c-mode-base-map (kbd "C-c C-c") 'compile)))
+            (setq-default c-default-style "linux" c-basic-offset 8)
+            (define-key c-mode-base-map (kbd "C-c C-c") 'compile)))
 
 (provide 'mookid-c)
 ;;; mookid-c.el ends here
