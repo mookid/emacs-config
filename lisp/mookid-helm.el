e99164c5585f09cbfc2967e1f6dcb09ec0470b84
mookid <nathan.moreau@m4x.org>
2015-12-09 02:17:02 +0100

    Secure load of dependencies.

--- lisp/mookid-helm.el
+++ lisp/mookid-helm.el
@@ -13,18 +13,20 @@
  (global-set-key (kbd "C-x m") 'helm-M-x)
  (helm-mode))
 
-(with-message
- "Loading shackle"
- (require 'shackle)
- (setq-default helm-display-function #'pop-to-buffer)
- (setq-default shackle-rules
-               '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :ratio 0.33)))
- (eval-after-load "init" (shackle-mode)))
+(eval-after-load 'helm
+  '(with-message
+    "Loading shackle"
+    (require 'shackle)
+    (setq-default helm-display-function #'pop-to-buffer)
+    (setq-default shackle-rules
+                  '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :ratio 0.33)))
+    (eval-after-load "init" (shackle-mode))))
 
-(with-message
- "Loading helm swoop"
- (require 'helm-swoop)
- (global-set-key (kbd "C-\\") 'helm-swoop-from-evil-search))
+(eval-after-load 'helm
+  '(with-message
+    "Loading helm swoop"
+    (require 'helm-swoop)
+    (global-set-key (kbd "C-\\") 'helm-swoop-from-evil-search)))
 
 (provide 'mookid-helm)
 ;;; mookid-helm.el ends here
