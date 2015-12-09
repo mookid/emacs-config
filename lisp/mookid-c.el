df5c93cdf1575dd8a5ff8c1abbb510d1ee4891be
mookid <nathan.moreau@m4x.org>
2015-12-09 01:28:56 +0100

    Name a lambda.

--- lisp/mookid-c.el
+++ lisp/mookid-c.el
@@ -4,10 +4,10 @@
 
 ;;; Code:
 (defvar c-mode-base-map)
-(add-hook 'c-initialization-hook
-          (lambda ()
-            (setq-default c-default-style "linux" c-basic-offset 8)
-            (define-key c-mode-base-map (kbd "C-c C-c") 'compile)))
+(defun c-setup ()
+  (setq-default c-default-style "linux" c-basic-offset 8)
+  (define-key c-mode-base-map (kbd "C-c C-c") 'compile))
+(add-hook 'c-initialization-hook 'c-setup)
 
 (provide 'mookid-c)
 ;;; mookid-c.el ends here
