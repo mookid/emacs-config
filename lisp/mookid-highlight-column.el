d9ca46d28a6fc6e11120a524cf28035ef7f4306d
mookid <nathan.moreau@m4x.org>
2015-12-10 09:46:30 +0100

    Remove autoload of highlight indentation.

--- lisp/mookid-highlight-column.el
+++ lisp/mookid-highlight-column.el
@@ -7,8 +7,6 @@
 
 (set-face-background 'highlight-indentation-current-column-face
                      "light steel blue")
-(add-hook 'prog-mode-hook
-          'highlight-indentation-current-column-mode)
 
 (provide 'mookid-highlight-column)
 ;;; mookid-highlight-column.el ends here
