2c8166687efd35753eb337913f372d9c8d4deb62
mookid <nathan.moreau@m4x.org>
2015-12-12 23:11:57 +0100

    Fix a typo.

--- lisp/mookid-helm.el
+++ lisp/mookid-helm.el
@@ -15,7 +15,7 @@
 
 (with-message "Loading shackle" (require 'mookid-shackle))
 (with-message "Loading helm swoop" (require 'mookid-helm-swoop))
-(with-message "Loading helm projectile" (require mookid-helm-projectile))
+(with-message "Loading helm projectile" (require 'mookid-helm-projectile))
 
 (provide 'mookid-helm)
 ;;; mookid-helm.el ends here
