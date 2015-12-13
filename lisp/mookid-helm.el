8443480ac5b99ff6322596094f6f1b4a7d631cb7
mookid <nathan.moreau@m4x.org>
2015-12-13 10:09:16 +0100

    Add a binding for helm-show-kill-ring

--- lisp/mookid-helm.el
+++ lisp/mookid-helm.el
@@ -9,6 +9,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
 (global-set-key (kbd "C-x m") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (helm-mode)
 
 (provide 'mookid-helm)
