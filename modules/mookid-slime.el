dc03c46e68009c3286e0b3e4052cdfbc09147ef8
mookid <nathan.moreau@m4x.org>
2015-12-18 00:34:01 +0100

    New module

---
+++ modules/mookid-slime.el
@@ -0,0 +1,10 @@
+;;; mookid-slime.el --- Configuration of slime
+
+;;; Commentary:
+
+;;; Code:
+(setq inferior-lisp-program "sbcl")
+(require 'slime-autoloads)
+
+(provide 'mookid-slime)
+;;; mookid-slime.el ends here
