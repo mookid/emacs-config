69d12140685c6c7e63e5046e60599e0b4676facf
mookid <nathan.moreau@m4x.org>
2015-12-17 15:43:36 +0100

    Add a binding for project switch.

--- modules/mookid-helm-projectile.el
+++ modules/mookid-helm-projectile.el
@@ -5,12 +5,13 @@
 ;;; Code:
 
 (with-eval-after-load 'helm
-   (require 'helm-projectile)
-   (require 'projectile)
-   (projectile-global-mode)
-   (setq-default projectile-indexing-method 'native)
-   (setq-default projectile-enable-caching t)
-   (global-set-key (kbd "<f5>") 'helm-projectile))
+  (require 'helm-projectile)
+  (require 'projectile)
+  (projectile-global-mode)
+  (setq-default projectile-indexing-method 'native)
+  (setq-default projectile-enable-caching t)
+  (global-set-key (kbd "<f5>") 'helm-projectile)
+  (global-set-key (kbd "<C-f5>") 'projectile-switch-project))
 
 (provide 'mookid-helm-projectile)
 ;;; mookid-helm-projectile.el ends here
