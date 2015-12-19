1a728bee84558364de70a055891fe1c557c6dff8
mookid <nathan.moreau@m4x.org>
2015-12-19 03:47:28 +0100

    Moving naked-emacs-congif to lisp directory.

--- lisp/mookid-naked-emacs-config.el
+++ lisp/mookid-naked-emacs-config.el
@@ -71,9 +71,9 @@ I add this hook because it seems that some package activates it."
 
 (with-message
  "Remove gui elements"
- (tooltip-mode -1)
- (tool-bar-mode -1)
- (menu-bar-mode -1)
+ (if (fboundp tooltip-mode) (tooltip-mode -1))
+ (if (fboundp tool-bar-mode) (toolbar-mode -1))
+ (if (fboundp menu-bar-mode) (menu-bar-mode -1))
  (if (fboundp 'scroll-bar-mode) (scroll-bar-mode +1))
  (blink-cursor-mode -1))
 
