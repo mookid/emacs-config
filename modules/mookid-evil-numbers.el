617fbe444ca753a13c8c8ef16c7a383e1f9fd225
mookid <nathan.moreau@m4x.org>
2015-12-19 19:16:39 +0100

    Fix.

--- modules/mookid-evil-numbers.el
+++ modules/mookid-evil-numbers.el
@@ -6,11 +6,10 @@
 (with-eval-after-load 'evil
   (require 'evil-numbers)
   (defvar evil-normal-state-map)
-  (cl-loop
-   for (key . val) in '((<f1> . evil-numbers/inc-at-pt)
-			(<f2> . evil-numbers/dec-at-pt))
-   do (define-key evil-normal-state-map
-	(kbd (format "C-%S" (symbol-name key))) val)))
+  (global-set-key (kbd (format "C-<f1>")) 'evil-numbers/inc-at-pt)
+  (global-set-key (kbd (format "C-<f2>")) 'evil-numbers/dec-at-pt))
 
 (provide 'mookid-evil-numbers)
 ;;; mookid-evil-numbers.el ends here
+
+(format )
