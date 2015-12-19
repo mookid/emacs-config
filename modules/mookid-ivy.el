c38f54654e7b278f255bf81c693f7171db89db6c
mookid <nathan.moreau@m4x.org>
2015-12-19 16:49:09 +0100

    Add a binding for ivy-alt-done (to make it behave more like helm).

--- modules/mookid-ivy.el
+++ modules/mookid-ivy.el
@@ -8,6 +8,7 @@
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
+(define-key ivy-minibuffer-map (kbd "<right>") 'ivy-alt-done)
 (setq ivy-use-virtual-buffers t)
 
 (provide 'mookid-ivy)
