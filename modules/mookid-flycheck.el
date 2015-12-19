496bfb14bef85cfc3322bed471ef4fcdb9d1c7d0
mookid <nathan.moreau@m4x.org>
2015-12-19 16:46:05 +0100

    Fix (did not compile).

--- modules/mookid-flycheck.el
+++ modules/mookid-flycheck.el
@@ -3,8 +3,9 @@
 ;;; Commentary:
 
 ;;; Code:
-(require 'flycheck)
-(define-key flycheck-mode-map (kbd "C-S-<next>") 'flycheck-next-error)
+(with-eval-after-load 'flycheck
+  (defvar flycheck-mode-map)
+  (define-key flycheck-mode-map (kbd "C-S-<next>") 'flycheck-next-error))
 
 (provide 'mookid-flycheck)
 ;;; mookid-flycheck.el ends here
