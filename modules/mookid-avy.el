c6dc5239d5bc2dd2f7ae3b3164c84d674e3e811c
mookid <nathan.moreau@m4x.org>
2015-12-19 18:14:10 +0100

    Fix: load map customizations after evil.

--- modules/mookid-avy.el
+++ modules/mookid-avy.el
@@ -4,12 +4,13 @@
 
 ;;; Code:
 (require 'avy)
-(setq-default avy-all-windows 'all-frames)
-(mapc (lambda (map)
-        (eval `(define-key ,map (kbd "s") 'avy-goto-word-or-subword-1)))
-      '(evil-motion-state-map
-        evil-visual-state-map
-        evil-normal-state-map))
+(with-eval-after-load 'evil
+  (setq-default avy-all-windows 'all-frames)
+  (mapc (lambda (map)
+	  (eval `(define-key ,map (kbd "s") 'avy-goto-word-or-subword-1)))
+	'(evil-motion-state-map
+	  evil-visual-state-map
+	  evil-normal-state-map)))
 
 (provide 'mookid-avy)
 ;;; mookid-avy.el ends here
