d31f1dba0d60a16aad68d4ef2da123c3cae8c6c6
mookid <nathan.moreau@m4x.org>
2015-12-19 04:01:03 +0100

    Revert .

--- lisp/mookid-naked-emacs-config.el
+++ lisp/mookid-naked-emacs-config.el
@@ -71,10 +71,10 @@ I add this hook because it seems that some package activates it."
 
 (with-message
  "Remove gui elements"
- (if (fboundp tooltip-mode) (tooltip-mode -1))
- (if (fboundp tool-bar-mode) (toolbar-mode -1))
- (if (fboundp menu-bar-mode) (menu-bar-mode -1))
- (if (fboundp 'scroll-bar-mode) (scroll-bar-mode +1))
+ (tooltip-mode -1)
+ (tool-bar-mode -1)
+ (menu-bar-mode -1)
+ (scroll-bar-mode +1)
  (blink-cursor-mode -1))
 
 (with-message
