3b2c788f335ed6ad31d6db4e65651da167658c82
mookid <nathan.moreau@m4x.org>
2015-12-19 11:13:22 +0100

    Add kill-ring command.

--- modules/mookid-ivy.el
+++ modules/mookid-ivy.el
@@ -6,6 +6,7 @@
 (require 'ivy)
 (ivy-mode 1)
 (global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (setq ivy-use-virtual-buffers t)
 
