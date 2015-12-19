9a93e3413f00607c5c833e0d8d885b7057e84964
mookid <nathan.moreau@m4x.org>
2015-12-19 11:14:03 +0100

    Cleanup.

--- init.el
+++ init.el
@@ -9,48 +9,55 @@
 (package-initialize)
 
 ;; Common lisp functionalities
-(with-no-warnings (require 'cl)) ; useless warning
+(require 'cl-lib)
 
-(setq load-path (cons "~/.emacs.d/lisp/" load-path))
+(defvar mookid-lisp-dir "~/.emacs.d/lisp"
+  "This is where are the non compiled lisp files.")
+(defvar mookid-modules-dir "~/.emacs.d/modules"
+  "This is where are the compiled lisp files.")
+
+(setq load-path (cons mookid-lisp-dir load-path))
 (autoload 'with-message "mookid-macros")
 (autoload 'with-title "mookid-macros")
 (autoload 'init-load "mookid-macros")
 
-(setq load-path (cons "~/.emacs.d/modules/" load-path))
+(add-to-list 'load-path mookid-modules-dir)
 
 (with-title "Naked emacs configuration" (require 'mookid-naked-emacs-config))
 
 ;; evil related packages
-(init-load (require 'mookid-evil))
-(init-load (require 'mookid-evil-numbers))
-(init-load (require 'mookid-evil-visualstar))
-(init-load (require 'mookid-evil-jumper))
-(init-load (require 'mookid-highlight-persist))
+(init-load 'mookid-evil)
+
+(init-load 'mookid-evil-numbers)
+(init-load 'mookid-evil-visualstar)
+(init-load 'mookid-evil-jumper)
+(init-load 'mookid-highlight-persist)
 ;; end
 
-(init-load (require 'mookid-powerline))
-(init-load (require 'mookid-rainbow))
-(init-load (require 'mookid-company))
+(init-load 'mookid-powerline)
+(init-load 'mookid-rainbow)
+(init-load 'mookid-company)
 
 ;; helm related packages
-(init-load (require 'mookid-helm))
-(init-load (require 'mookid-shackle))
-(init-load (require 'mookid-helm-swoop))
-(init-load (require 'mookid-helm-projectile))
+;;(init-load 'mookid-ivy)
+(init-load 'mookid-helm)
+(init-load 'mookid-shackle)
+(init-load 'mookid-helm-swoop)
+(init-load 'mookid-helm-projectile)
 ;; end
 
-(init-load (require 'mookid-smartparens))
-(init-load (require 'mookid-avy))
-(init-load (require 'mookid-flycheck))
+(init-load 'mookid-smartparens)
+(init-load 'mookid-avy)
+(init-load 'mookid-flycheck)
 
 ;; ocaml settings
-(init-load (require 'mookid-tuareg))
-(init-load (require 'mookid-ocp-indent))
-;; (init-load (require 'mookid-merlin))
+(init-load 'mookid-tuareg)
+(init-load 'mookid-ocp-indent)
+;; (init-load 'mookid-merlin)
 ;; end
 
-(init-load (require 'mookid-c))
-(init-load (require 'mookid-highlight-column))
+(init-load 'mookid-c)
+(init-load 'mookid-highlight-column)
 
 (with-message
  "Loading private settings"
