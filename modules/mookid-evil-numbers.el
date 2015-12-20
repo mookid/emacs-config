d022677b623933a161b4e579bbf586ce922d1f93
mookid <nathan.moreau@m4x.org>
2015-12-20 14:42:56 +0100

    Remove useless format.

--- modules/mookid-evil-numbers.el
+++ modules/mookid-evil-numbers.el
@@ -6,8 +6,8 @@
 (with-eval-after-load 'evil
   (require 'evil-numbers)
   (defvar evil-normal-state-map)
-  (global-set-key (kbd (format "C-<f1>")) 'evil-numbers/inc-at-pt)
-  (global-set-key (kbd (format "C-<f2>")) 'evil-numbers/dec-at-pt))
+  (global-set-key (kbd "C-<f1>") 'evil-numbers/inc-at-pt)
+  (global-set-key (kbd "C-<f2>") 'evil-numbers/dec-at-pt))
 
 (provide 'mookid-evil-numbers)
 ;;; mookid-evil-numbers.el ends here
