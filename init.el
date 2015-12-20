5ea8c663e4765606b8a8a30d4b0ae1f6541992b2
mookid <nathan.moreau@m4x.org>
2015-12-20 01:28:01 +0100

    Fix broken macro.

--- init.el
+++ init.el
@@ -68,3 +68,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters tuareg smartparens smart-mode-line-powerline-theme slime shackle rainbow-blocks paredit ocp-indent merlin highlight-indentation helm-projectile golden-ratio flycheck find-file-in-project evil-visualstar evil-search-highlight-persist evil-numbers evil-jumper counsel company ace-isearch 0blayout))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- modules/mookid-smartparens.el
+++ modules/mookid-smartparens.el
@@ -13,20 +13,20 @@
 (global-set-key (kbd "M-[") 'sp-backward-unwrap-sexp)
 (require 'cl-lib)
 (cl-macrolet
- ((customize (key val)
-	  (let ((symb (intern (format "wrap-with-%Ss" key)))
-		(kbinding (format "C-c %s" val)))
-	    (progn
-	      `(defun ,symb (&optional arg)
-		 "Wrap the next form (or selection) using `sp-wrap-with-pair'."
-		 (interactive "P")
-		 (sp-wrap-with-pair ,val))
-	      `(global-set-key (kbd ,kbinding) ',symb)))
-	   (customize paren    "(")
-	   (customize bracket  "[")
-	   (customize brace    "{")
-	   (customize squote   "'")
-	   (customize dquote   "\""))))
+    ((customize (key val)
+		(let ((symb (intern (format "wrap-with-%Ss" key)))
+		      (kbinding (format "C-c %s" val)))
+		  `(progn
+		     (defun ,symb (&optional arg)
+		       "Wrap the next form (or selection) using `sp-wrap-with-pair'."
+		       (interactive "P")
+		       (sp-wrap-with-pair ,val))
+		     (global-set-key (kbd ,kbinding) ',symb)))))
+  (customize paren   "(")
+  (customize bracket "[")
+  (customize brace   "{")
+  (customize squote  "'")
+  (customize dquote  "\""))
 
 (provide 'mookid-smartparens)
 ;;; mookid-smartparens.el ends here
