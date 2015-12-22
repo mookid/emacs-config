1749edfaf7d27728f71a41b5fc6ecdd966c9feef
mookid <nathan.moreau@m4x.org>
2015-12-22 15:18:47 +0100

    Add rainbow delimiters to slime repl.

--- modules/mookid-slime.el
+++ modules/mookid-slime.el
@@ -5,6 +5,7 @@
 ;;; Code:
 (require 'slime-autoloads)
 (setq-default inferior-lisp-program "sbcl")
+(add-hook 'comint-mode-hook 'rainbow-delimiters-mode)
 
 (provide 'mookid-slime)
 ;;; mookid-slime.el ends here
