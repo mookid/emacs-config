3b8528bbe21fff36729523324650155765dec036
mookid <nathan.moreau@m4x.org>
2015-12-29 22:45:55 +0100

    Add orgmode module.

---
+++ modules/mookid-orgmode.el
@@ -0,0 +1,13 @@
+;;; mookid-orgmode --- Configuration for orgmode
+
+;;; Commentary:
+
+;;; Code:
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (ocaml . t)))
+
+(provide 'mookid-orgmode)
+;;; mookid-orgmode.el ends here
