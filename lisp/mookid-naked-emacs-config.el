9c740d41bb93fc05cde229451d3923694f76164d
mookid <nathan.moreau@m4x.org>
2016-01-01 16:58:27 +0100

    Change a binding.

--- lisp/mookid-naked-emacs-config.el
+++ lisp/mookid-naked-emacs-config.el
@@ -165,7 +165,7 @@ See `toggle-selective-display' and `increase-selective-display'."
 (with-message
  "Window switch bindings"
  (global-set-key (kbd "<f2> <f2>") 'swap-windows)
- (global-set-key (kbd "C-x 4") 'toggle-window-split)
+ (global-set-key (kbd "<f2> <f1>") 'toggle-window-split)
  (defun swap-windows ()
    "If you have 2 windows, it swaps them."
    (interactive)
