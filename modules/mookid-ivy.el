d772100333a75ef3cbe1c7022ace4e74479b5ddc
mookid <nathan.moreau@m4x.org>
2016-01-05 10:46:43 +0100

    Fail module loading if counsel is not installed.

--- modules/mookid-ivy.el
+++ modules/mookid-ivy.el
@@ -5,13 +5,15 @@
 ;;; Code:
 (require 'ivy)
 (ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+
+(require 'counsel)
 (global-set-key (kbd "M-s") 'swiper)
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (define-key ivy-minibuffer-map (kbd "<right>") 'ivy-alt-done)
 (define-key ivy-minibuffer-map (kbd "<left>") 'ivy-backward-delete-char)
 (global-set-key (kbd "C-x m") 'counsel-M-x)
-(setq ivy-use-virtual-buffers t)
 
 (provide 'mookid-ivy)
 ;;; mookid-ivy.el ends here
