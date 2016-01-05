f6169c5b31cb9dfe2b9b6ba673e2e42c91b22f66
mookid <nathan.moreau@m4x.org>
2016-01-05 10:24:44 +0100

    Preserving default bindings.

--- modules/mookid-ivy.el
+++ modules/mookid-ivy.el
@@ -5,12 +5,12 @@
 ;;; Code:
 (require 'ivy)
 (ivy-mode 1)
-(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-s") 'swiper)
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (define-key ivy-minibuffer-map (kbd "<right>") 'ivy-alt-done)
 (define-key ivy-minibuffer-map (kbd "<left>") 'ivy-backward-delete-char)
-(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x m") 'counsel-M-x)
 (setq ivy-use-virtual-buffers t)
 
 (provide 'mookid-ivy)
