1df1ffe05f0d78f97cdae5cca52bb223773bbe25
mookid <nathan.moreau@m4x.org>
2016-01-07 16:35:57 +0100

    Add a binding to insert stars.

--- modules/mookid-tuareg.el
+++ modules/mookid-tuareg.el
@@ -16,9 +16,16 @@
     (assq-delete-all str prettify-symbols-alist)))
 (add-hook 'tuareg-mode-hook 'remove-some-prettifiers)
 
+(defun insert-stars ()
+  "Insert a line with stars."
+  (interactive)
+  (insert (concat "(*************************************"
+		  "**************************************)")))
+
 (defun tuareg-evil-bindings ()
   "Bindings for tuareg mode depending on evil mode."
-  (global-set-key (kbd "C-'") 'tuareg-eval-region))
+  (global-set-key (kbd "C-'") 'tuareg-eval-region)
+  (global-set-key (kbd "C-+") 'insert-stars))
 (defun define-tuareg-bindings ()
   "Keybindings for tuareg mode."
   (add-hook 'evil-mode-hook 'tuareg-evil-bindings))
