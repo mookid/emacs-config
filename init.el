2d97817a45c369ba35368c653d4f2819de29902e
mookid <nathan.moreau@m4x.org>
2016-01-09 23:24:24 +0100

    Cleanup.

--- init.el
+++ init.el
@@ -50,14 +50,6 @@
 (init-load 'slime)
 (init-load 'expand-region)
 
-;; helm related packages
-;; (init-load 'helm)
-;; (init-load 'shackle)
-;; (init-load 'helm-swoop)
-;; (init-load 'helm-projectile)
-;; end
-
-(init-load 'smartparens)
 (init-load 'avy)
 (init-load 'flycheck)
 
@@ -68,24 +60,9 @@
 ;; end
 
 (init-load 'c)
-;; (init-load 'highlight-column)
 
 (with-message
  "Loading private settings"
  (let ((f "~/.emacs.d/private.el")) (when (file-exists-p f) (load f))))
 
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (tuareg smartparens slime rainbow-delimiters rainbow-blocks ocp-indent flycheck find-file-in-project expand-region evil-visualstar evil-search-highlight-persist evil-numbers evil-jumper counsel company avy))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
