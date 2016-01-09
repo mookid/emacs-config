1aaca4a5db4acc3e1b899456a33b187c8b1bc275
mookid <nathan.moreau@m4x.org>
2016-01-09 22:44:51 +0100

    Numbers bindings only in normal mode.

--- modules/mookid-evil-numbers.el
+++ modules/mookid-evil-numbers.el
@@ -5,8 +5,9 @@
 ;;; Code:
 (with-eval-after-load 'evil
   (require 'evil-numbers)
-  (global-set-key (kbd "C-<f1>") 'evil-numbers/inc-at-pt)
-  (global-set-key (kbd "C-<f2>") 'evil-numbers/dec-at-pt))
+  (defvar evil-normal-state-map)
+  (define-key evil-normal-state-map (kbd "C-<f1>") 'evil-numbers/inc-at-pt)
+  (define-key evil-normal-state-map (kbd "C-<f2>") 'evil-numbers/dec-at-pt))
 
 (provide 'mookid-evil-numbers)
 ;;; mookid-evil-numbers.el ends here
