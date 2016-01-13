4847bc07af87533d9ac6fa179655b94b5c2edae3
mookid <nathan.moreau@m4x.org>
2016-01-13 10:54:32 +0100

    Add ocp-indent.

---
+++ elpa/ocp-indent-20150914.132/ocp-indent-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ocp-indent" "20150914.132" "automatic indentation with ocp-indent" 'nil :url "http://www.typerex.org/ocp-indent.html" :keywords '("ocaml" "languages"))
