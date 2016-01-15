c6209113502a346c1eb17871b40af745117a62e5
mookid <nathan.moreau@m4x.org>
2016-01-15 23:18:45 +0100

    Update.

--- elpa/evil-jumper-20151228.1146/evil-jumper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-jumper" "20151228.1146" "Jump like vimmers do!" '((evil "0") (cl-lib "0.5")) :url "https://github.com/bling/evil-jumper" :keywords '("evil" "vim" "jumplist" "jump" "list"))
