7877348c1730affce6b20d9d90b3fce21a550107
mookid <nathan.moreau@m4x.org>
2016-01-18 09:44:31 +0100

    Define a variable for c indentation.

--- modules/mookid-c.el
+++ modules/mookid-c.el
@@ -4,11 +4,13 @@
 
 ;;; Code:
 (defvar c-mode-base-map)
+(defvar c-indentation 8 "The indentation for C code.")
 (defun c-setup ()
   "My setup for C."
-  (setq-default c-default-style "linux" c-basic-offset 8)
+  (setq-default c-default-style "linux" c-basic-offset c-indentation)
   (define-key c-mode-base-map (kbd "C-c C-c") 'compile)
   (define-key c-mode-base-map (kbd "C-c C-a") 'ff-find-other-file)
+  (setq-default indent-tabs-mode nil)
   (require 'clang-format))
 
 (add-hook 'c-initialization-hook 'c-setup)
