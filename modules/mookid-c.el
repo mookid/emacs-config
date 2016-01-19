f05e8997de3adb726184ae3c07ad066f95f807ee
mookid <nathan.moreau@m4x.org>
2016-01-19 10:14:50 +0100

    Define per mode separators.

--- modules/mookid-c.el
+++ modules/mookid-c.el
@@ -5,11 +5,19 @@
 ;;; Code:
 (defvar c-mode-base-map)
 (defvar c-indentation 8 "The indentation for C code.")
+(defvar c-stars "/*****************************************************************************/"
+  "A separator for C code.")
+(defun c-insert-stars ()
+  "Insert the value of `c-stars'."
+  (interactive)
+  (insert c-stars))
+
 (defun c-setup ()
   "My setup for C."
   (setq-default c-default-style "linux" c-basic-offset c-indentation)
   (define-key c-mode-base-map (kbd "C-c C-c") 'compile)
   (define-key c-mode-base-map (kbd "C-c C-a") 'ff-find-other-file)
+  (define-key c-mode-base-map (kbd "C-c =") 'c-insert-stars)
   (setq-default indent-tabs-mode nil)
   (require 'clang-format))
 
