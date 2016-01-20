d4c4b6396aa334233cf3d240c315294c8c51d804
mookid <nathan.moreau@m4x.org>
2016-01-20 23:33:05 +0100

    Speedup the startup with gc-cons-threshold to max.

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 ;; My Emacs config, with simple options.
 
 ;;; Code:
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
 ;; The configuration directories
 (defvar mookid-root-dir "~/.emacs.d"
   "The root directory of the configuration.")
@@ -14,58 +18,11 @@
 
 (defvar mookid-modules-dir (expand-file-name "modules" mookid-root-dir)
   "This is where the configuration of the Lisp files are.")
-;; end
 
 (add-to-list 'load-path mookid-lisp-dir)
 (add-to-list 'load-path mookid-modules-dir)
 
-(autoload 'with-message "mookid-macros")
-(autoload 'with-title "mookid-macros")
-(autoload 'init-load "mookid-macros")
-
-(with-message
- "Loading packages list"
- (require 'package)
- (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
- (package-initialize))
-
-(with-title "Naked emacs configuration" (require 'mookid-naked-emacs-config))
-
-;; evil related packages
-(init-load 'evil)
-(init-load 'evil-numbers)
-(init-load 'evil-visualstar)
-(init-load 'evil-jumper)
-(init-load 'highlight-persist)
-;; end
-
-(init-load 'rainbow)
-(init-load 'company)
-(init-load 'elisp-slime-nav)
-
-;; ivy related packages
-(init-load 'ivy)
-(init-load 'find-file-in-project)
-;; end
-
-(init-load 'slime)
-(init-load 'expand-region)
-
-(init-load 'avy)
-(init-load 'ace-window)
-(init-load 'flycheck)
-
-;; ocaml settings
-(init-load 'tuareg)
-(init-load 'ocp-indent)
-;; (init-load 'merlin)
-;; end
-
-(init-load 'c)
-
-(let ((f (expand-file-name "private.el" mookid-root-dir)))
-  (when (file-exists-p f)
-    (with-message "Loading private settings" (load f))))
+(let ((gc-cons-threshold most-positive-fixnum)) (require 'mookid-init))
 
 (provide 'init)
 ;;; init.el ends here
