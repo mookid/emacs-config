e5a4e02a683be6068e249adb02e1ca23f40dcc49
mookid <nathan.moreau@m4x.org>
2016-01-20 13:20:43 +0100

    Add ace-window module.

---
+++ modules/mookid-ace-window.el
@@ -0,0 +1,10 @@
+;;; mookid-ace-window --- Configuration for ace-window
+
+;;; Commentary:
+
+;;; Code:
+(require 'ace-window)
+(global-set-key (kbd "M-o") 'ace-window)
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(provide 'mookid-ace-window)
+;;; mookid-ace-window.el ends here
