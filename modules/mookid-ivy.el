561faf98a90666904a71e63174a3a78ef2eb975a
mookid <nathan.moreau@m4x.org>
2016-01-23 00:31:34 +0100

    Change a few bindings.

--- modules/mookid-ivy.el
+++ modules/mookid-ivy.el
@@ -8,12 +8,15 @@
 (setq ivy-use-virtual-buffers t)
 
 (require 'counsel)
-(global-set-key (kbd "M-s") 'swiper)
+(global-set-key (kbd "M-s") 'swiper-from-isearch)
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
 (define-key ivy-minibuffer-map (kbd "<right>") 'ivy-alt-done)
 (define-key ivy-minibuffer-map (kbd "<left>") 'ivy-backward-delete-char)
 (global-set-key (kbd "C-x m") 'counsel-M-x)
+
+(defvar evil-normal-state-map)
+(define-key evil-normal-state-map (kbd "C-b") 'ivy-switch-buffer)
+(define-key evil-normal-state-map (kbd "C-f") 'counsel-find-file)
 
 (provide 'mookid-ivy)
 ;;; mookid-ivy.el ends here
