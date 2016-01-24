c3edd7afc318e7320e635e476d6f4a547f03f99a
mookid <nathan.moreau@m4x.org>
2016-01-24 23:51:48 +0100

    Cosmetic.

--- lisp/mookid-naked-emacs-config.el
+++ lisp/mookid-naked-emacs-config.el
@@ -112,29 +112,31 @@ I add this hook because it seems that some package activates it."
 (with-message
  "Setting up selective display"
  (let ((depth 1))
-   (global-set-key (kbd "<f6>") 'toggle-selective-display)
-   (global-set-key (kbd "C-<f6>") 'increase-selective-display)
-   (global-set-key (kbd "S-<f6>") 'decrease-selective-display)
-   (defun toggle-selective-display ()
-     "Hide lines starting with a lot of spaces.
+   (global-set-key (kbd "<f6>")
+		   (defun toggle-selective-display ()
+		     "Hide lines starting with a lot of spaces.
 
 See `increase-selective-display' to increase the number of spaces.
 See `decrease-selective-display' to decrease it."
-     (interactive)
-     (set-selective-display (unless selective-display depth)))
+		     (interactive)
+		     (set-selective-display (unless selective-display depth))))
    (cl-flet ((g (offset)
-                (setq depth (+ depth offset))
-                (set-selective-display depth)))
-     (defun increase-selective-display ()
-       "Increase the cap for `toogle-selective-display'.
+		(setq depth (+ depth offset))
+		(set-selective-display depth)))
+     (global-set-key (kbd "C-<f6>")
+		     (defun increase-selective-display ()
+		       "Increase the cap for `toogle-selective-display'.
 
 See `toggle-selective-display' and `decrease-selective-display'."
-       (interactive) (when (< depth 20) (g 1)))
-     (defun decrease-selective-display ()
-       "Decrease the cap for `toogle-selective-display'.
+		       (interactive)
+		       (when (< depth 20) (g 1))))
+     (global-set-key (kbd "S-<f6>")
+		     (defun decrease-selective-display ()
+		       "Decrease the cap for `toogle-selective-display'.
 
 See `toggle-selective-display' and `increase-selective-display'."
-       (interactive) (when (> depth 1) (g -1))))))
+		       (interactive)
+		       (when (> depth 1) (g -1)))))))
 
 (with-message
  "Configuring parenthesis settings"
