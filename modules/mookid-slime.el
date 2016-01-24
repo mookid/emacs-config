16bb3b7f0af15e5fa0fbef2890e8fedba309c458
mookid <nathan.moreau@m4x.org>
2016-01-24 16:32:58 +0100

    Local hyperspec.

--- modules/mookid-slime.el
+++ modules/mookid-slime.el
@@ -6,6 +6,7 @@
 (require 'slime-autoloads)
 (setq-default inferior-lisp-program "sbcl")
 (add-hook 'comint-mode-hook 'rainbow-delimiters-mode)
+(setq-default common-lisp-hyperspec-root "file://C:/Hyperspec/")
 
 (provide 'mookid-slime)
 ;;; mookid-slime.el ends here
