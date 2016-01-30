38fdc4efd96419338660ad9b7fa3e37f4f74a52c
mookid <nathan.moreau@m4x.org>
2016-01-30 21:58:18 +0100

    Update + install lispy.

--- elpa/elisp-slime-nav-20150805.1248/elisp-slime-nav-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "elisp-slime-nav" "20150805.1248" "Make M-. and M-, work in elisp like they do in slime" 'nil :url "https://github.com/purcell/elisp-slime-nav" :keywords '("navigation" "slime" "elisp" "emacs-lisp"))
