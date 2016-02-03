068d2d2b9c2f8bf89553eacd3c9b07ccd60e01ee
mookid <nathan.moreau@m4x.org>
2016-02-03 18:56:25 +0100

    Add cool bindings for evil window functions.

--- modules/mookid-ace-window.el
+++ modules/mookid-ace-window.el
@@ -6,7 +6,10 @@
 (require 'ace-window)
 (autoload 'bind-key-non-insert-mode "mookid-evil")
 (with-eval-after-load 'evil
-  (bind-key-non-insert-mode (kbd "'") 'ace-window))
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (bind-key-non-insert-mode (kbd "'") 'ace-window)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (setq aw-dispatch-alist (cons '(?v evil-window-vsplit) aw-dispatch-alist))
+  (setq aw-dispatch-alist (cons '(?h evil-window-split) aw-dispatch-alist))
+  (setq aw-dispatch-alist (cons '(?c evil-window-delete) aw-dispatch-alist)))
 (provide 'mookid-ace-window)
 ;;; mookid-ace-window.el ends here
