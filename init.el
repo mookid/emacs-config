356f0f4d53e8840ce6cfddb317cf412e8042a234
mookid <nathan.moreau@m4x.org>
2016-02-04 11:58:35 +0100

    Update.

--- init.el
+++ init.el
@@ -26,3 +26,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (tuareg smooth-scrolling slime rainbow-delimiters rainbow-blocks ocp-indent loccur lispy gnuplot-mode flycheck find-file-in-project expand-region evil-visualstar evil-search-highlight-persist evil-numbers evil-jumper evil-anzu elisp-slime-nav d-mode counsel company clang-format))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
