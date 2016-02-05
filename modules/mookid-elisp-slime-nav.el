8051ad56b00b2891bddd4e1b415c6a9d7c31285f
mookid <nathan.moreau@m4x.org>
2016-02-05 22:41:17 +0100

    Change from describe to find in elisp-nav.

--- modules/mookid-elisp-slime-nav.el
+++ modules/mookid-elisp-slime-nav.el
@@ -7,9 +7,11 @@
 (require 'elisp-slime-nav)
 
 (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+(add-hook 'lisp-interaction-mode 'elisp-slime-nav-mode)
+
 (with-eval-after-load 'evil
   (evil-define-key 'normal emacs-lisp-mode-map
-    "\"" 'elisp-slime-nav-describe-elisp-thing-at-point))
+    "\"" 'elisp-slime-nav-find-elisp-thing-at-point))
 
 (provide 'mookid-elisp-slime-nav)
 ;;; mookid-elisp-slime-nav.el ends here
