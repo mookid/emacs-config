3fc95930f6083b50555f9b9701653beec7b14c13
mookid <nathan.moreau@m4x.org>
2016-02-08 23:32:37 +0100

    Set evil-shift-width for ocaml.

--- modules/mookid-tuareg.el
+++ modules/mookid-tuareg.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 
 ;; With prettify-symbols-mode on, there is by default some ugly prettifiers.
-;; I remove them. Customizations is possible by editing the list in the
+;; I remove them.  Customizations is possible by editing the list in the
 ;; remove-some-prettifiers function.
 
 ;;; Code:
@@ -31,6 +31,8 @@
   (define-key tuareg-mode-map (kbd "C-c =") 'tuareg-insert-stars))
 (add-hook 'tuareg-mode-hook 'remove-some-prettifiers)
 (add-hook 'tuareg-mode-hook 'define-tuareg-bindings)
+(with-eval-after-load 'evil
+  (setq-default evil-shift-width 2))
 
 (provide 'mookid-tuareg)
 ;;; mookid-tuareg.el ends here
