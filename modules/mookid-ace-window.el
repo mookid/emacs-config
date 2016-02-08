5cff9544b95caf2c67318d03500d5fac42eeb18c
mookid <nathan.moreau@m4x.org>
2016-02-08 23:23:37 +0100

    Change evil-window-split binding.

--- modules/mookid-ace-window.el
+++ modules/mookid-ace-window.el
@@ -9,7 +9,7 @@
   (bind-key-non-insert-mode (kbd "'") 'ace-window)
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   (setq aw-dispatch-alist (cons '(?v evil-window-vsplit) aw-dispatch-alist))
-  (setq aw-dispatch-alist (cons '(?h evil-window-split) aw-dispatch-alist))
+  (setq aw-dispatch-alist (cons '(?b evil-window-split) aw-dispatch-alist))
   (setq aw-dispatch-alist (cons '(?c evil-window-delete) aw-dispatch-alist)))
 (provide 'mookid-ace-window)
 ;;; mookid-ace-window.el ends here
