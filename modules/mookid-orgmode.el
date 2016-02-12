bb4a5b70df578d30bc42429130b3e746f4002e2a
mookid <nathan.moreau@m4x.org>
2016-02-12 16:41:06 +0100

    Configure beamer export in org-mode.

--- modules/mookid-orgmode.el
+++ modules/mookid-orgmode.el
@@ -9,5 +9,16 @@
  '((emacs-lisp . t)
    (ocaml . t)))
 
+(require 'ox-latex)
+(add-to-list 'org-latex-classes
+             '("beamer"
+               "\\documentclass\[presentation\]\{beamer\}"
+               ("\\section\{%s\}" . "\\section*\{%s\}")
+               ("\\subsection\{%s\}" . "\\subsection*\{%s\}")
+               ("\\subsubsection\{%s\}" . "\\subsubsection*\{%s\}")))
+
+
+(setq org-latex-create-formula-image-program 'dvipng)
+
 (provide 'mookid-orgmode)
 ;;; mookid-orgmode.el ends here
