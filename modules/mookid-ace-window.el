0de7bac984c5177caf1f2996aea841469c565f2c
mookid <nathan.moreau@m4x.org>
2016-02-15 15:20:14 +0100

    Always dispatch ace-window.

--- modules/mookid-ace-window.el
+++ modules/mookid-ace-window.el
@@ -8,6 +8,7 @@
 (with-eval-after-load 'evil
   (bind-key-non-insert-mode (kbd "'") 'ace-window)
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (setq aw-dispatch-always t)
   (setq aw-dispatch-alist (cons '(?v evil-window-vsplit) aw-dispatch-alist))
   (setq aw-dispatch-alist (cons '(?b evil-window-split) aw-dispatch-alist))
   (setq aw-dispatch-alist (cons '(?c evil-window-delete) aw-dispatch-alist)))
