c1e7bc1f8ac0b512a72801cc5c8f9a8ff12e5e2e
mookid <nathan.moreau@m4x.org>
2016-02-15 19:03:34 +0100

    Add module ocp-index.

---
+++ modules/mookid-ocp-index.el
@@ -0,0 +1,30 @@
+;;; mookid-ocp-index --- Configuration for ocp-index
+
+;;; Commentary:
+
+;;; Code:
+(require 'ocp-index)
+(defvar tuareg-mode-map)
+(defun ocp-index-jump-to-definition-at-point-maybe-other-window (p)
+  (interactive "P")
+  (if p
+      (ocp-index-jump-to-definition-at-point-other-window)
+    (ocp-index-jump-to-definition-at-point)))
+
+(defun ocp-index-jump-to-sig-at-point-maybe-other-window (p)
+  (interactive "P")
+  (if p
+      (ocp-index-jump-to-sig-at-point-other-window)
+    (ocp-index-jump-to-sig-at-point)))
+
+(defun ocp-index-setup ()
+  "My setup for ocp-index."
+  (with-eval-after-load 'evil
+    (define-key evil-normal-state-map (kbd "C-c t") 'caml-types-show-type)
+    (define-key evil-normal-state-map (kbd "C-.") 'ocp-index-jump-to-definition-at-point-maybe-other-window)
+    (define-key evil-normal-state-map (kbd "M-.") 'ocp-index-jump-to-sig-at-point-maybe-other-window)))
+
+(add-hook 'tuareg-mode-hook 'ocp-index-setup)
+
+(provide 'mookid-ocp-index)
+;;; mookid-ocp-index.el ends here
