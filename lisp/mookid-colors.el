fd0a0b4a028f6fec5f70ef7e6467c67df471ad85
mookid <nathan.moreau@m4x.org>
2016-02-28 23:46:34 +0100

    Add some faces customizations.

--- lisp/mookid-colors.el
+++ lisp/mookid-colors.el
@@ -12,6 +12,10 @@
 (set-face-attribute 'font-lock-string-face nil :foreground "orange red")
 (set-face-attribute 'font-lock-keyword-face nil :weight 'bold)
 (set-face-attribute 'font-lock-function-name-face nil :foreground "violet")
+(set-face-attribute 'region nil :background "grey17")
+
+(set-face-attribute 'eldoc-highlight-function-argument nil :inherit 'default)
+(set-face-attribute 'help-argument-name nil :inherit 'default)
 
 (provide 'mookid-colors)
 ;;; mookid-colors.el ends here
