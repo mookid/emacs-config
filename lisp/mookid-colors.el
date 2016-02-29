d5fd8f5ee8bedc61a2fc5f5e5c6f6010074995eb
mookid <nathan.moreau@m4x.org>
2016-02-29 18:47:29 +0100

    More colors tweaking.

--- lisp/mookid-colors.el
+++ lisp/mookid-colors.el
@@ -11,9 +11,15 @@
 (set-face-attribute 'font-lock-doc-face nil :foreground "coral")
 (set-face-attribute 'font-lock-string-face nil :foreground "orange red")
 (set-face-attribute 'font-lock-type-face nil :foreground "orange red")
-(set-face-attribute 'font-lock-keyword-face nil :weight 'bold)
+(set-face-attribute 'font-lock-keyword-face nil
+                    :weight 'bold
+                    :foreground "deep sky blue")
+(set-face-attribute 'font-lock-builtin-face nil :foreground "deep sky blue")
+
 (set-face-attribute 'font-lock-function-name-face nil :foreground "violet")
 (set-face-attribute 'region nil :background "grey17")
+(set-face-attribute 'match nil :foreground "blue")
+(set-face-attribute 'compilation-info nil :foreground "deep sky blue")
 
 (set-face-attribute 'eldoc-highlight-function-argument nil :inherit 'default)
 (set-face-attribute 'help-argument-name nil :inherit 'default)
