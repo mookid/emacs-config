08e1ade38b82c3cc666876a9aee790ddf5ca23aa
mookid <nathan.moreau@m4x.org>
2016-02-29 18:47:09 +0100

    Add hungry-delete.

---
+++ modules/mookid-hungry-delete-mode.el
@@ -0,0 +1,10 @@
+;;; mookid-hungry-delete --- Configuration for humgry-delete
+
+;;; Commentary:
+;; From http://endlessparentheses.com/hungry-delete-mode.html
+
+;;; Code:
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+(provide 'mookid-hungry-delete)
+;;; mookid-tuareg.el ends here
