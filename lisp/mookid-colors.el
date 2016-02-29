dd2d2387c6a5f7b6c7735161f46dc26eab8f51fa
mookid <nathan.moreau@m4x.org>
2016-02-29 21:33:32 +0100

    Try fix a problem at startup.

--- lisp/mookid-colors.el
+++ lisp/mookid-colors.el
@@ -5,24 +5,25 @@
 
 ;;; Code:
 (load-theme 'leuven)
-(set-background-color "grey4")
-(set-foreground-color "ivory2")
-(set-face-attribute 'font-lock-variable-name-face nil :foreground "coral")
-(set-face-attribute 'font-lock-doc-face nil :foreground "coral")
-(set-face-attribute 'font-lock-string-face nil :foreground "orange red")
-(set-face-attribute 'font-lock-type-face nil :foreground "orange red")
-(set-face-attribute 'font-lock-keyword-face nil
-                    :weight 'bold
-                    :foreground "deep sky blue")
-(set-face-attribute 'font-lock-builtin-face nil :foreground "deep sky blue")
+(with-eval-after-load 'init
+  (set-background-color "grey4")
+  (set-foreground-color "ivory2")
+  (set-face-attribute 'font-lock-variable-name-face nil :foreground "coral")
+  (set-face-attribute 'font-lock-doc-face nil :foreground "coral")
+  (set-face-attribute 'font-lock-string-face nil :foreground "orange red")
+  (set-face-attribute 'font-lock-type-face nil :foreground "orange red")
+  (set-face-attribute 'font-lock-keyword-face nil
+		      :weight 'bold
+		      :foreground "deep sky blue")
+  (set-face-attribute 'font-lock-builtin-face nil :foreground "deep sky blue")
 
-(set-face-attribute 'font-lock-function-name-face nil :foreground "violet")
-(set-face-attribute 'region nil :background "grey17")
-(set-face-attribute 'match nil :foreground "blue")
-(set-face-attribute 'compilation-info nil :foreground "deep sky blue")
+  (set-face-attribute 'font-lock-function-name-face nil :foreground "violet")
+  (set-face-attribute 'region nil :background "grey17")
+  (set-face-attribute 'match nil :foreground "blue")
+  (set-face-attribute 'compilation-info nil :foreground "deep sky blue")
 
-(set-face-attribute 'eldoc-highlight-function-argument nil :inherit 'default)
-(set-face-attribute 'help-argument-name nil :inherit 'default)
+  (set-face-attribute 'eldoc-highlight-function-argument nil :inherit 'default)
+  (set-face-attribute 'help-argument-name nil :inherit 'default))
 
 (provide 'mookid-colors)
 ;;; mookid-colors.el ends here
