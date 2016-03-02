e220c504d48b777cc7b02f56b211ed7859c758aa
mookid <nathan.moreau@m4x.org>
2016-03-02 02:03:50 +0100

    Use wombat.

--- lisp/mookid-colors.el
+++ lisp/mookid-colors.el
@@ -4,26 +4,15 @@
 ;; Configuration of Emacs colors.
 
 ;;; Code:
-(load-theme 'leuven)
-(with-eval-after-load 'init
-  (set-background-color "grey4")
-  (set-foreground-color "ivory2")
-  (set-face-attribute 'font-lock-variable-name-face nil :foreground "coral")
-  (set-face-attribute 'font-lock-doc-face nil :foreground "coral")
-  (set-face-attribute 'font-lock-string-face nil :foreground "orange red")
-  (set-face-attribute 'font-lock-type-face nil :foreground "orange red")
-  (set-face-attribute 'font-lock-keyword-face nil
-		      :weight 'bold
-		      :foreground "deep sky blue")
-  (set-face-attribute 'font-lock-builtin-face nil :foreground "deep sky blue")
-
-  (set-face-attribute 'font-lock-function-name-face nil :foreground "violet")
-  (set-face-attribute 'region nil :background "grey17")
-  (set-face-attribute 'match nil :foreground "blue")
-  (set-face-attribute 'compilation-info nil :foreground "deep sky blue")
-
-  (set-face-attribute 'eldoc-highlight-function-argument nil :inherit 'default)
-  (set-face-attribute 'help-argument-name nil :inherit 'default))
+(load-theme 'wombat)
+(defvar *default-light-color* nil "Foreground for most faces.")
+(setq *default-light-color* "cornsilk1")
+(let ((theme-default-foreground (face-attribute 'default :foreground)))
+  (mapc (lambda (face)
+	  (when (string= (face-attribute face :foreground)
+			 theme-default-foreground)
+	    (set-face-foreground face *default-light-color*)))
+	(face-list)))
 
 (provide 'mookid-colors)
 ;;; mookid-colors.el ends here
