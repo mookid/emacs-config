c2d994d4585295aa6ceac1b6c82c64f990fc7965
mookid <nathan.moreau@m4x.org>
2016-03-02 22:43:17 +0100

    Dead code.

--- modules/mookid-ivy.el
+++ modules/mookid-ivy.el
@@ -20,18 +20,5 @@
 (define-key evil-normal-state-map (kbd "<C-return>") 'counsel-find-file)
 (define-key evil-normal-state-map (kbd "SPC") 'counsel-M-x)
 
-(require 'imenu)
-(defun ivy-imenu-get-candidates-from (alist  &optional prefix)
-  (cl-loop for elm in alist
-           nconc (if (imenu--subalist-p elm)
-                     (ivy-imenu-get-candidates-from
-                      (cl-loop for (e . v) in (cdr elm) collect
-                               (cons e (if (integerp v) (copy-marker v) v)))
-                      (concat prefix (if prefix ".") (car elm)))
-                   (and (cdr elm) ; bug in imenu, should not be needed.
-                        (setcdr elm (copy-marker (cdr elm))) ; Same as [1].
-                        (list (cons (concat prefix (if prefix ".") (car elm))
-                                    (copy-marker (cdr elm))))))))
-
 (provide 'mookid-ivy)
 ;;; mookid-ivy.el ends here
