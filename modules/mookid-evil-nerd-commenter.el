642d5001bc14f1411315f2a9ac67d10056baa670
mookid <nathan.moreau@m4x.org>
2016-03-02 02:35:04 +0100

    Add a module for evil-nerd-commenter.

---
+++ modules/mookid-evil-nerd-commenter.el
@@ -0,0 +1,10 @@
+;;; mookid-evil-nerd-commenter --- Configuration for evil-nerd-commenter
+
+;;; Commentary:
+
+;;; Code:
+(require 'evil-nerd-commenter)
+(evilnc-default-hotkeys)
+
+(provide 'mookid-evil-nerd-commenter)
+;;; mookid-evil-nerd-commenter.el ends here
