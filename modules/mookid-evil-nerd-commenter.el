3a625e727c63f7631243822f0d5f7ca552c0cf8d
mookid <nathan.moreau@m4x.org>
2016-03-02 03:00:54 +0100

    toggle-line-comment with evilnc commands.

--- modules/mookid-evil-nerd-commenter.el
+++ modules/mookid-evil-nerd-commenter.el
@@ -4,7 +4,12 @@
 
 ;;; Code:
 (require 'evil-nerd-commenter)
-(evilnc-default-hotkeys)
-
+(with-eval-after-load 'evil
+  (defvar evil-normal-state-map)
+  (defvar evil-visual-state-map)
+  (define-key evil-normal-state-map (kbd ",") 'evilnc-comment-operator)
+  (define-key evil-visual-state-map (kbd ",") 'evilnc-comment-operator)
+  (define-key evil-normal-state-map (kbd "M-,") 'evilnc-copy-and-comment-lines)
+  (define-key evil-visual-state-map (kbd "M-,") 'evilnc-copy-and-comment-lines))
 (provide 'mookid-evil-nerd-commenter)
 ;;; mookid-evil-nerd-commenter.el ends here
