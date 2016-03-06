47cbdbf0a0ba0c60c56268c5b7f64c8a1396d957
mookid <nathan.moreau@m4x.org>
2016-03-06 11:56:52 +0100

    Add a loccur package.

---
+++ modules/mookid-loccur.el
@@ -0,0 +1,11 @@
+;;; mookid-loccur --- Configuration for loccur
+
+;;; Commentary:
+
+;;; Code:
+(require 'loccur)
+
+(global-set-key (kbd "M-s p") 'loccur-current)
+
+(provide 'mookid-loccur)
+;;; mookid-loccur.el ends here
