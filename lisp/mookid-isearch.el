053c79f432e78bb9c8a131d0bd63eca63039b645
Nathan <Nathan@localhost.localdomain>
2016-03-14 00:47:30 +0100

    Add isearch-{beginning-end}-of-buffer.

--- lisp/mookid-isearch.el
+++ lisp/mookid-isearch.el
@@ -8,5 +8,20 @@
 (define-key isearch-mode-map (kbd "<left>") 'isearch-delete-char)
 (define-key isearch-mode-map (kbd "<right>") 'isearch-yank-word-or-char)
 
+(define-key isearch-mode-map "\M-<" 'isearch-beginning-of-buffer)
+(define-key isearch-mode-map "\M->" 'isearch-end-of-buffer)
+
+(defun isearch-beginning-of-buffer ()
+  "Move isearch point to the beginning of the buffer."
+  (interactive)
+  (goto-char (point-min))
+  (isearch-repeat-forward))
+
+(defun isearch-end-of-buffer ()
+  "Move isearch point to the end of the buffer."
+  (interactive)
+  (goto-char (point-max))
+  (isearch-repeat-backward))
+
 (provide 'mookid-isearch)
 ;;; mookid-isearch.el ends here
