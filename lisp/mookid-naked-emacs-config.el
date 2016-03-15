a5744a0a74f4e01c50d208b7a6e488237965c698
mookid <nathan.moreau@m4x.org>
2016-03-15 15:49:09 +0100

    Add a binding for navigation in dired.

--- lisp/mookid-naked-emacs-config.el
+++ lisp/mookid-naked-emacs-config.el
@@ -283,9 +283,10 @@ already narrowed."
          (if (and transient-mark-mode mark-active) (region-end))))
     (error "Invalid syntax")))
 
-(require dired-x)
-(with-eval-after-load 'evil
-  (define-key evil-normal-state-map (kbd "g j") 'dired-jump))
+(with-eval-after-load 'mookid-evil
+  (define-key evil-normal-state-map (kbd "g j") 'dired-jump)
+  (define-key dired-mode-map (kbd "j") 'dired-jump)
+  (evil-set-initial-state 'dired-mode 'emacs))
 
 (provide 'mookid-naked-emacs-config)
 ;;; mookid-naked-emacs-config.el ends here
