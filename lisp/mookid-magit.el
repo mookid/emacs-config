a639247dfb6208a7be77850893b9e897973ea34f
mookid <nathan.moreau@m4x.org>
2016-03-17 01:19:07 +0100

    Add a module for magit.

---
+++ lisp/mookid-magit.el
@@ -0,0 +1,10 @@
+;;; mookid-magit --- Configuration of magit.
+
+;;; Commentary:
+
+;;; Code:
+(require 'magit)
+
+(global-set-key (kbd "<f7>") 'magit-status)
+(provide 'mookid-magit)
+;;; mookid-magit.el ends here
