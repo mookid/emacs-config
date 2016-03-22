139caeb6b0f2a389b44242758f3c5ac8cdf0e115
mookid <nathan.moreau@m4x.org>
2016-03-22 11:49:21 +0100

    Add binding to other-window.

--- modules/mookid-ace-window.el
+++ modules/mookid-ace-window.el
@@ -5,11 +5,17 @@
 ;;; Code:
 (require 'ace-window)
 (autoload 'bind-key-non-insert-mode "mookid-evil")
+(defun mookid-other-window ()
+  "Call `other-window' with argument 1."
+  (interactive)
+  (other-window 1))
+
 (with-eval-after-load 'evil
   (bind-key-non-insert-mode (kbd "'") 'ace-window)
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   (setq aw-dispatch-always t)
   (setq aw-dispatch-alist (cons '(?v evil-window-vsplit) aw-dispatch-alist))
+  (setq aw-dispatch-alist (cons '(?\' mookid-other-window) aw-dispatch-alist))
   (setq aw-dispatch-alist (cons '(?b evil-window-split) aw-dispatch-alist))
   (setq aw-dispatch-alist (cons '(?c evil-window-delete) aw-dispatch-alist)))
 (provide 'mookid-ace-window)
