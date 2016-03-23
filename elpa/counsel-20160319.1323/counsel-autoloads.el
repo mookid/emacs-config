4102941950c1fbc2fd51529e486ea90d87aa283b
mookid <nathan.moreau@m4x.org>
2016-03-23 14:52:08 +0100

    Use the melpa version of counsel.

--- elpa/counsel-20160319.1323/counsel-autoloads.el
+++ elpa/counsel-20160319.1323/counsel-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "counsel" "counsel.el" (22253 26265 0 0))
+;;;### (autoloads nil "counsel" "counsel.el" (22258 40759 0 0))
 ;;; Generated autoloads from counsel.el
 
 (autoload 'counsel-el "counsel" "\
