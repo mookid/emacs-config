3531ea5258064cabe7a76b11354e33b8cf4a93ea
mookid <nathan.moreau@m4x.org>
2016-03-24 03:09:49 +0100

    Cleanup.

--- modules/mookid-tuareg.el
+++ modules/mookid-tuareg.el
@@ -10,12 +10,6 @@
 (autoload 'tuareg-mode "tuareg")
 (add-to-list 'auto-mode-alist '("\\.ml[ily]?$" . tuareg-mode))
 
-(defun remove-some-prettifiers ()
-  "Remove undesirable prettifiers."
-  (dolist (str '("||" "&&" "not"))
-    (assq-delete-all str prettify-symbols-alist)))
-(add-hook 'tuareg-mode-hook 'remove-some-prettifiers)
-
 (defvar tuareg-mode-map)
 (defvar ocaml-stars "(***************************************************************************)"
   "A separator for OCaml code.")
@@ -34,31 +28,6 @@
 (add-hook 'tuareg-mode-hook 'define-tuareg-bindings)
 (with-eval-after-load 'evil
   (setq-default evil-shift-width 2))
-
-(defun verticalize-sequence (pos1 pos2)
-  "Transform a one-line sequence literal to a multi-line."
-  (interactive "r")
-  (if (use-region-p)
-      (save-restriction
-	(narrow-to-region pos1 pos2)
-	(goto-char (point-min))
-	(forward-char)
-	(insert "\n")
-	(indent-according-to-mode)
-	(while (< 2 (- (point-max) (point)))
-	  (forward-sexp)
-	  (if (equal "," (thing-at-point 'char))
-	      (progn
-		(forward-char)
-		(insert "\n")
-		(indent-according-to-mode)
-		)))
-	;; add a newline before the closing delimiter
-	(goto-char  (point-max))
-	(backward-char)
-	(insert "\n")
-	)
-    (message "You need to highlight a sequence.")))
 
 (provide 'mookid-tuareg)
 ;;; mookid-tuareg.el ends here
