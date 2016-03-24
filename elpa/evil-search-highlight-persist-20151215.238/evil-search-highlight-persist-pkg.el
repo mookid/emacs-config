f94174c68228351b9bf30b1b5b509c5d50d44488
mookid <nathan.moreau@m4x.org>
2016-03-25 00:00:34 +0100

    Add highlight persist again (not quite ready yet...).

---
+++ elpa/evil-search-highlight-persist-20151215.238/evil-search-highlight-persist-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-search-highlight-persist" "20151215.238" "Persistent highlights after search" '((highlight "0")))
--- init.el
+++ init.el
@@ -26,3 +26,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (evil-search-highlight-persist use-package tuareg smooth-scrolling slime ruby-block rainbow-mode rainbow-delimiters rainbow-blocks projectile ocp-indent magit loccur lispy hungry-delete gnuplot-mode fullframe flycheck expand-region evil-surround evil-numbers evil-nerd-commenter evil-anzu elisp-slime-nav d-mode counsel company clang-format))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
