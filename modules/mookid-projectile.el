a2cd62f6079261794fc33148ba0baf0385cc68be
mookid <nathan.moreau@m4x.org>
2016-03-25 10:45:50 +0100

    Customize projectile.

--- modules/mookid-projectile.el
+++ modules/mookid-projectile.el
@@ -6,15 +6,16 @@
 (require 'projectile)
 
 (projectile-global-mode)
-(setq-default projectile-indexing-method 'native)
-(setq-default projectile-enable-caching t)
+(setq projectile-indexing-method 'native)
+(setq projectile-enable-caching t)
+(setq projectile-mode-line '(:eval (concat " <" (projectile-project-name) ">")))
 (defun mookid-projectile (p)
   "My projectile command.
 
 If P is non nil, call `projectile-find-file' else call `projectile-switch-project'."
   (interactive "P")
   (if p (projectile-switch-project) (projectile-find-file)))
-(global-set-key (kbd "<M-S-return>") 'mookid-projectile)
+(global-set-key (kbd "<C-S-return>") 'mookid-projectile)
 
 (setq projectile-completion-system 'ivy)
 
