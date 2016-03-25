b9c43a5778b2a7cdcfe6ce5eded21742b6fbd68d
mookid <nathan.moreau@m4x.org>
2016-03-25 11:02:48 +0100

    Cleanup.

--- modules/mookid-evil.el
+++ modules/mookid-evil.el
@@ -14,35 +14,41 @@
 (defvar evil-normal-state-map)
 
 (require 'cl-lib)
-(defun gensetq (mode-colors-alist)
-  (cl-loop for (mode . custom) in mode-colors-alist
-	   for color = (symbol-name (cdr (assoc :color custom)))
-	   for shape = (cdr (assoc :cursor-shape custom))
-	   append `(,(intern (format "evil-%S-state-cursor" mode))
-		    ',(list color shape)
-		    ,(intern (format "evil-%S-state-tag" mode))
-		    nil)))
-
-(defun gencond (mode-colors-alist)
-  (cl-loop for (mode . custom) in mode-colors-alist
-	   for color = (symbol-name (cdr (assoc :color custom)))
-	   for foreground = (let ((symb (cdr (assoc :foreground custom))))
-			      (when symb (symbol-name symb)))
-	   collect
-	   `((,(intern (format "evil-%S-state-p" mode)))
-	     '(,color ,foreground))))
 
 (defmacro customize (&rest mode-colors-alist)
-  `(progn
-     (setq ,@(gensetq mode-colors-alist))
-     (add-hook 'post-command-hook
-	       (lambda ()
-		 (let* ((colors (cond ,@(gencond mode-colors-alist)))
-			(background (car colors))
-			(foreground-opt (cdr colors)))
-		   (set-face-background 'mode-line background)
-		   (when foreground-opt
-		     (set-face-foreground 'mode-line (car foreground-opt))))))))
+     "Customization code for evil.
+
+Expects MODE-COLORS-ALIST with elements of the form (MODE . CUSTOMIZATIONS).
+The currently used CUSTOMIZATIONS are:
+* color
+* cursor shape"
+     (cl-flet ((gensetq (mode-colors-alist)
+			(cl-loop for (mode . custom) in mode-colors-alist
+				 for color = (symbol-name (cdr (assoc :color custom)))
+				 for shape = (cdr (assoc :cursor-shape custom))
+				 append `(,(intern (format "evil-%S-state-cursor" mode))
+					  ',(list color shape)
+					  ,(intern (format "evil-%S-state-tag" mode))
+					  nil)))
+
+	       (gencond (mode-colors-alist)
+			(cl-loop for (mode . custom) in mode-colors-alist
+				 for color = (symbol-name (cdr (assoc :color custom)))
+				 for foreground = (let ((symb (cdr (assoc :foreground custom))))
+						    (when symb (symbol-name symb)))
+				 collect
+				 `((,(intern (format "evil-%S-state-p" mode)))
+				   '(,color ,foreground)))))
+       `(progn
+	  (setq ,@(gensetq mode-colors-alist))
+	  (add-hook 'post-command-hook
+		    (lambda ()
+		      (let* ((colors (cond ,@(gencond mode-colors-alist)))
+			     (background (car colors))
+			     (foreground-opt (cdr colors)))
+			(set-face-background 'mode-line background)
+			(when foreground-opt
+			  (set-face-foreground 'mode-line (car foreground-opt)))))))))
 
 (customize
  (insert . ((:color . red) (:foreground . white) (:cursor-shape . bar)))
