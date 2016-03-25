5f12d0f097e6dcdf1c0d21b9d0190d3cce5ed541
mookid <nathan.moreau@m4x.org>
2016-03-25 11:02:58 +0100

    Fix.

--- modules/mookid-evil-highlight-persist.el
+++ modules/mookid-evil-highlight-persist.el
@@ -4,11 +4,7 @@
 
 ;;; Code:
 (with-eval-after-load 'evil
-  (require 'evil-search-highlight-persist)
+  (require 'evil-search-highlight-persist))
 
-  (face-rem)
-  (setq evil-search-highlight-persist-highlight-face nil)
-
-  (global-evil-search-highlight-persist t)
-  (add-hook 'isearch-mode-end-hook
-	    'evil-search-highlight-persist-remo
+(provide 'mookid-evil-highlight-persist)
+;;; mookid-evil-highlight-persist.el ends here
