f14441470773e2717ceba2499fb2a381a1a87cb1
mookid <nathan.moreau@m4x.org>
2016-04-02 17:53:56 +0200

    Make popups appear on the side.

--- modules/mookid-popwin.el
+++ modules/mookid-popwin.el
@@ -5,6 +5,10 @@
 ;;; Code:
 (require 'popwin)
 (popwin-mode 1)
-(remove '*vc-diff* popwin:special-display-config)
+
+(setq popwin:popup-window-position 'right)
+(setq popwin:adjust-other-windows nil)
+(setq popwin:popup-window-width 60)
+
 (provide 'mookid-popwin)
 ;;; mookid-popwin.el ends here
