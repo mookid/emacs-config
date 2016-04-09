55ed6b826e22a9619f9a0b59a65b9968fb25de23
mookid <nathan.moreau@m4x.org>
2016-04-10 00:56:39 +0200

    Remove evil dependency in mookid-ace-window.

--- modules/mookid-ace-window.el
+++ modules/mookid-ace-window.el
@@ -12,9 +12,14 @@
 (define-key global-map (kbd "M-p") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 (setq aw-dispatch-always t)
-(setq aw-dispatch-alist (cons '(?v evil-window-vsplit) aw-dispatch-alist))
-(setq aw-dispatch-alist (cons '(?\' mookid-other-window) aw-dispatch-alist))
-(setq aw-dispatch-alist (cons '(?b evil-window-split) aw-dispatch-alist))
-(setq aw-dispatch-alist (cons '(?c evil-window-delete) aw-dispatch-alist))
+(setq aw-dispatch-alist (cons '(?v split-window-right) aw-dispatch-alist))
+(setq aw-dispatch-alist (cons '(?p mookid-other-window) aw-dispatch-alist))
+(setq aw-dispatch-alist (cons '(?b split-window-below) aw-dispatch-alist))
+(setq aw-dispatch-alist (cons '(?c delete-window) aw-dispatch-alist))
+
+;; Auto balance windows:
+(mapc (lambda (fun) (advice-add fun :after #'balance-windows))
+      '(split-window-right split-window-below delete-window))
+
 (provide 'mookid-ace-window)
 ;;; mookid-ace-window.el ends here
