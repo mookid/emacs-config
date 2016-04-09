ff7e3d4d06667a394b4f1ae540e00bb828483edf
mookid <nathan.moreau@m4x.org>
2016-04-10 01:33:06 +0200

    Cosmetic.

--- lisp/mookid-mouse.el
+++ lisp/mookid-mouse.el
@@ -5,19 +5,19 @@
 
 ;;; Code:
 (require 'mouse)
-(global-set-key (kbd "<S-mouse-1>") 'mouse-set-mark)
+(define-key global-map (kbd "<S-mouse-1>") 'mouse-set-mark)
 (setq mouse-drag-copy-region t)
 (setq mouse-yank-at-point t)
 
 (setq mouse-autoselect-window t)
 
 (require 'mouse-copy)
-(global-set-key (kbd "<C-down-mouse-1>") 'mouse-drag-secondary-pasting)
-(global-set-key (kbd "<C-S-down-mouse-1>") 'mouse-drag-secondary-moving)
+(define-key global-map (kbd "<C-down-mouse-1>") 'mouse-drag-secondary-pasting)
+(define-key global-map (kbd "<C-S-down-mouse-1>") 'mouse-drag-secondary-moving)
 
 (setq mouse-drag-copy-region t)
-(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
-(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+(define-key global-map (kbd "<C-wheel-up>") 'text-scale-increase)
+(define-key global-map (kbd "<C-wheel-down>") 'text-scale-decrease)
 
 (defun click-to-isearch (click)
   "Start `isearch-forward-symbol-at-point' on CLICK."
@@ -25,21 +25,21 @@
   (goto-char (posn-point (event-start click)))
   (isearch-forward-symbol-at-point))
 
-(global-set-key (kbd "<mouse-3>") 'click-to-isearch)
+(define-key global-map (kbd "<mouse-3>") 'click-to-isearch)
 
 (define-key isearch-mode-map (kbd "<mouse-3>") 'isearch-repeat-forward)
 
-(global-set-key (kbd "<mode-line> <down-mouse-1>") 'enlarge-window)
+(define-key global-map (kbd "<mode-line> <down-mouse-1>") 'enlarge-window)
 
 (global-unset-key (kbd "<mode-line> <mouse-2>"))
-(global-set-key (kbd "<mode-line> <down-mouse-2>") 'delete-other-windows-vertically)
+(define-key global-map (kbd "<mode-line> <down-mouse-2>") 'delete-other-windows-vertically)
 
 (require 'mouse-drag)
 (defun mouse-drag-throw-vertically (start-event)
   "Similar to `mouse-drag-throw' but only vertically.
 
 To test this function, evaluate:
-    (global-set-key [down-mouse-2] \\='mouse-drag-throw-vertically)"
+    (define-key global-map [down-mouse-2] \\='mouse-drag-throw-vertically)"
   (interactive "e")
   (save-selected-window
     (let* ((start-posn (event-start start-event))
@@ -63,7 +63,7 @@ To test this function, evaluate:
 		     (eq (car-safe event) 'switch-frame)))
 	  (when (eq start-window (posn-window end))
 	    (setq scroll-delta (mouse-drag-scroll-delta (- start-row row))))
-	  
+
 	  (if (or (/= 0 scroll-delta)
 		  (/= 0 scroll-col-delta))
 	      (progn
