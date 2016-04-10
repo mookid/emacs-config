499844a7c16af4a15c3562e4ced08612a6665662
mookid <nathan.moreau@m4x.org>
2016-04-10 20:02:10 +0200

    More namespace hygiene.

--- modules/mookid-tuareg.el
+++ modules/mookid-tuareg.el
@@ -7,28 +7,27 @@
 (add-to-list 'auto-mode-alist '("\\.ml[ily]?$" . tuareg-mode))
 
 (defvar tuareg-mode-map)
-(defvar ocaml-stars "(***************************************************************************)"
+(defvar mookid-ocaml-stars "(***************************************************************************)"
   "A separator for OCaml code.")
-(defun tuareg-insert-stars ()
+(defun mookid-tuareg-insert-stars ()
   "Insert a line with stars."
   (interactive)
   (newline)
-  (insert ocaml-stars)
+  (insert mookid-ocaml-stars)
   (newline))
 
-(defun define-tuareg-bindings ()
+(defun mookid-tuareg-setup ()
   "Keybindings for tuareg mode."
   (define-key tuareg-mode-map (kbd "C-'") 'tuareg-eval-region)
-  (define-key tuareg-mode-map (kbd "C-c =") 'tuareg-insert-stars))
+  (define-key tuareg-mode-map (kbd "C-c =") 'mookid-tuareg-insert-stars))
 
-(add-hook 'tuareg-mode-hook 'define-tuareg-bindings)
+(add-hook 'tuareg-mode-hook 'mookid-tuareg-setup)
 
 (with-eval-after-load 'caml
   (mapc (lambda (face)
 	  (when (string-prefix-p "caml-types" (face-name face))
 	    (set-face-attribute face nil
 				:background "deep pink"
-				:weight 'bold
 				:foreground "white")))
 	(face-list)))
 
