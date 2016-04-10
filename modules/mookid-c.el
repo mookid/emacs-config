cc02274049ef337739d198c9015c8dd4bdf2fd2e
mookid <nathan.moreau@m4x.org>
2016-04-10 20:02:10 +0200

    More namespace hygiene.

--- modules/mookid-c.el
+++ modules/mookid-c.el
@@ -7,17 +7,19 @@
 (defvar c-indentation 8 "The indentation for C code.")
 (defvar c-stars "/*****************************************************************************/"
   "A separator for C code.")
-(defun c-insert-stars ()
+(defun mookid-c-insert-stars ()
   "Insert the value of `c-stars'."
   (interactive)
   (insert c-stars))
 
+(require 'find-file)
+(require 'compile)
 (defun c-setup ()
   "My setup for C."
   (setq-default c-default-style "linux" c-basic-offset c-indentation)
   (define-key c-mode-base-map (kbd "C-c C-c") 'compile)
   (define-key c-mode-base-map (kbd "C-c C-a") 'ff-find-other-file)
-  (define-key c-mode-base-map (kbd "C-c =") 'c-insert-stars)
+  (define-key c-mode-base-map (kbd "C-c =") 'mookid-c-insert-stars)
   (setq-default indent-tabs-mode nil))
 
 (add-hook 'c-initialization-hook 'c-setup)
