776181ae5a492624a9c53eea2618b2e0d45b104a
mookid <nathan.moreau@m4x.org>
2016-04-11 22:41:57 +0200

    Rebind count-yank-pop.

--- modules/mookid-ivy.el
+++ modules/mookid-ivy.el
@@ -14,7 +14,7 @@
 (define-key ivy-minibuffer-map (kbd "<right>") 'ivy-alt-done)
 (define-key ivy-minibuffer-map (kbd "<left>") 'ivy-backward-delete-char)
 
-(define-key global-map (kbd "M-y") 'counsel-yank-pop)
+(define-key global-map (kbd "C-M-y") 'counsel-yank-pop)
 (define-key global-map (kbd "M-x") 'counsel-M-x)
 (define-key global-map (kbd "C-x <return>") 'counsel-M-x)
 (define-key global-map (kbd "C-x m") 'counsel-M-x)
@@ -22,7 +22,7 @@
 (define-key global-map (kbd "<C-return>") 'counsel-find-file)
 
 (add-hook 'grep-setup-hook
-	  (lambda () (define-key grep-mode-map (kbd "RET") 'ivy-switch-buffer)))
+          (lambda () (define-key grep-mode-map (kbd "RET") 'ivy-switch-buffer)))
 
 (provide 'mookid-ivy)
 ;;; mookid-ivy.el ends here
