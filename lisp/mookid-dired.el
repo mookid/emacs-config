c9e3d2a4a287e8aad764b3e735a1d15b5ebff729
mookid <nathan.moreau@m4x.org>
2016-04-21 14:34:21 +0200

    Remapping in global map.

--- lisp/mookid-dired.el
+++ lisp/mookid-dired.el
@@ -9,17 +9,17 @@
 (define-key dired-mode-map (kbd "M-<left>") 'dired-jump)
 (define-key dired-mode-map (kbd "M-<right>") 'dired-find-file)
 
-(define-key global-map (kbd "M-<left>") 'dired-jump)
-(define-key global-map (kbd "M-<right>") 'mookid-find-thing)
+(define-key global-map (kbd "C-M-<left>") 'dired-jump)
+(define-key global-map (kbd "C-M-<right>") 'mookid-find-thing)
 
 (defun mookid-find-thing ()
   "Find variable, function or file at point."
   (interactive)
   (cond ((not (eq (variable-at-point) 0))
-	 (call-interactively 'describe-variable))
-	((function-called-at-point)
-	 (call-interactively 'describe-function))
-	(t (find-file-at-point))))
+         (call-interactively 'describe-variable))
+        ((function-called-at-point)
+         (call-interactively 'describe-function))
+        (t (find-file-at-point))))
 
 (provide 'mookid-dired)
 ;;; mookid-dired.el ends here
