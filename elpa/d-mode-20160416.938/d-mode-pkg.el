978388f8d9d9f86d2956a368c727ecd231fcc82e
mookid <nathan.moreau@m4x.org>
2016-04-24 15:57:19 +0200

    Packages update.

---
+++ elpa/d-mode-20160416.938/d-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "d-mode" "20160416.938" "D Programming Language major mode for (X)Emacs" 'nil :keywords '("d" "programming" "language" "emacs" "cc-mode"))
