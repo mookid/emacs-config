e46104261654550e584f8c10441b182a69480ff6
mookid <nathan.moreau@m4x.org>
2016-06-23 03:14:07 +0200

    Simplify.

--- mookid-init.el
+++ mookid-init.el
@@ -43,15 +43,9 @@ The return value reports success or failure."
       (setq lst (cdr lst)))
     lst))
 
-(defun mookid-move-beginning-of-line (arg)
+(defun mookid-move-beginning-of-line (_arg)
   "Redefinition of `move-beginning-of-line'."
   (interactive "^p")
-  (setq arg (or arg 1))
-
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
   (let ((orig-point (point)))
     (back-to-indentation)
     (when (= orig-point (point))
