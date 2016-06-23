0706679e08efba48ab2e560b985653b8b3b11beb
mookid <nathan.moreau@m4x.org>
2016-06-23 03:12:46 +0200

    Improve show-paren-mode.

--- mookid-init.el
+++ mookid-init.el
@@ -169,9 +169,21 @@ The return value reports success or failure."
  (defvar electric-pair-pairs)
  (defvar show-paren-delay)
  (electric-pair-mode t)
- (setq electric-pair-pairs '((?\{ . ?\})))
+ (add-to-list 'electric-pair-pairs '(?\{ . ?\}))
  (setq show-paren-delay 0)
  (show-paren-mode t)
+ (defadvice show-paren-function
+     (after show-matching-paren-offscreen activate)
+   "Show the matching line in the echo area.
+
+Has no effect if the character before point is not of the syntax
+class ')'."
+   (interactive)
+   (let* ((cb (char-before (point)))
+          (matching-text (and cb
+                              (char-equal (char-syntax cb) ?\) )
+                              (blink-matching-open))))
+     (when matching-text (message matching-text))))
  (set-face-background 'show-paren-match "turquoise"))
 
 (mookid-with-message
