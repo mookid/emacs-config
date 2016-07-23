6fa221d42c309ce7fbda92506567fc5c806fa168
mookid <nathan.moreau@m4x.org>
2016-07-23 14:45:32 +0200

    Cleanup.

--- init.el
+++ init.el
@@ -20,17 +20,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (rust-mode whole-line-or-region use-package tuareg spacemacs-theme smooth-scrolling smooth-scroll smart-mode-line slime ruby-block ranger rainbow-mode rainbow-delimiters rainbow-blocks projectile powerline pos-tip plan9-theme ocp-indent magit loccur lispy image+ icicles hungry-delete haskell-mode gnuplot-mode fullframe flycheck find-file-in-project expand-region evil-nerd-commenter elisp-slime-nav d-mode counsel company clang-format centered-cursor-mode caml benchmark-init base16-theme anzu))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
