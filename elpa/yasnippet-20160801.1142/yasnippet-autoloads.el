e35cba285996a3671ba9d88e370351d21e902a88
mookid <nathan.moreau@m4x.org>
2016-08-18 11:25:37 +0200

    Fix yasnippets; add amap.

--- elpa/yasnippet-20160801.1142/yasnippet-autoloads.el
+++ elpa/yasnippet-20160801.1142/yasnippet-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "yasnippet" "yasnippet.el" (22452 24798 0 0))
+;;;### (autoloads nil "yasnippet" "yasnippet.el" (22453 31686 0 0))
 ;;; Generated autoloads from yasnippet.el
 
 (autoload 'yas-minor-mode "yasnippet" "\
@@ -50,7 +50,7 @@ A mode for editing yasnippets
 
 ;;;***
 
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (22452 24793 0 0))
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (22453 31683 0 0))
 
 ;;;***
 
