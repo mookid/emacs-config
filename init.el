4c0440c2f63fb83eb256893ee850090ad500860c
mookid <nathan.moreau@m4x.org>
2016-09-10 13:40:28 +0200

    Move directory stuff in init.el.

--- init.el
+++ init.el
@@ -15,8 +15,20 @@
 (defvar my-root-dir "~/.emacs.d"
   "The root directory of the configuration.")
 
+(defvar custom-file)
+(defvar savehist-file)
+(setq custom-file (expand-file-name ".emacs-custom.el" my-root-dir))
+(setq savehist-file (expand-file-name "savehist" my-root-dir))
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name "backups" my-root-dir))))
+
 (let ((gc-cons-threshold most-positive-fixnum))
-  (load-file (expand-file-name "my-init.el" my-root-dir)))
+  (load-file (expand-file-name "my-init.el" my-root-dir))
+  (let ((private-file (expand-file-name "private.el" my-root-dir)))
+    (when (file-exists-p private-file)
+      (condition-case nil
+          (load private-file)
+        (error (message "Error during loading of private settings"))))))
 
 (provide 'init)
 ;;; init.el ends here
