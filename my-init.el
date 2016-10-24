7e5ac90767b8c9df08fa7265f3941e110be85ef1
mookid <nathan.moreau@m4x.org>
2016-10-24 09:51:48 +0200

    Remove my-narrow-dwim.

--- my-init.el
+++ my-init.el
@@ -351,33 +351,6 @@ With a prefix argument ARG, insert `file:' before."
   (insert (concat (if arg "file:" "")
                   (buffer-file-name (my-previous-buffer)))))
 
-;; from http://endlessparentheses.com/emacs-narrow-or-widen-dwim.html:
-(defun my-narrow-dwim (p)
-  "Widen if buffer is narrowed, narrow-dwim otherwise.
-Dwim means: region, org-src-block, org-subtree, or defun,
-whichever applies first.  Narrowing to org-src-block actually
-calls `org-edit-src-code'.
-
-With prefix P, don't widen, just narrow even if buffer is
-already narrowed."
-  (interactive "P")
-  (declare (interactive-only))
-  (cond ((and (buffer-narrowed-p) (not p)) (widen))
-        ((region-active-p)
-         (narrow-to-region (region-beginning) (region-end)))
-        ;; ((derived-mode-p 'org-mode)
-        ;;  ;; `org-edit-src-code' is not a real narrowing
-        ;;  ;; command. Remove this first conditional if you
-        ;;  ;; don't want it.
-        ;;  (cond ((ignore-errors (org-edit-src-code))
-        ;;         (delete-other-windows))
-        ;;        ((ignore-errors (org-narrow-to-block) t))
-        ;;        (t (org-narrow-to-subtree))))
-        ;; ((derived-mode-p 'latex-mode)
-        ;;  (LaTeX-narrow-to-environment))
-        (t (narrow-to-defun))))
-(define-key global-map (kbd "C-x n n") 'my-narrow-dwim)
-
 (defun my-join-line (beg end)
   "If the range BEG END is active, group it on one line.
 Otherwise, join the current line with the following."
