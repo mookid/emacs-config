b91519e117dd04efdae11545e021d72845f491a4
mookid <nathan.moreau@m4x.org>
2016-11-02 22:06:20 +0100

    Disable GC for the whole loading.
    collections: 7 -> 1 => ~7% speedup

--- init.el
+++ init.el
@@ -5,31 +5,29 @@
 ;; My Emacs config, with simple options.
 
 ;;; Code:
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
+(let ((gc-cons-threshold most-positive-fixnum)
+      (debug-on-error t))
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (package-initialize)
 
-(require 'benchmark-init)
+  (require 'benchmark-init)
 
-;; The configuration directories
-(defvar custom-file)
-(defvar savehist-file)
-(defvar yas-snippet-dirs)
-(setq custom-file (expand-file-name ".emacs-custom.el" user-emacs-directory))
-(setq savehist-file (expand-file-name "savehist" user-emacs-directory))
-(setq yas-snippet-dirs (expand-file-name "snippets" user-emacs-directory))
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name "backups" user-emacs-directory))))
-
-(setq debug-on-error t)
-(let ((gc-cons-threshold most-positive-fixnum))
+  ;; The configuration directories
+  (defvar custom-file)
+  (defvar savehist-file)
+  (defvar yas-snippet-dirs)
+  (setq custom-file (expand-file-name ".emacs-custom.el" user-emacs-directory))
+  (setq savehist-file (expand-file-name "savehist" user-emacs-directory))
+  (setq yas-snippet-dirs (expand-file-name "snippets" user-emacs-directory))
+  (setq backup-directory-alist
+        `(("." . ,(expand-file-name "backups" user-emacs-directory))))
   (load-file (expand-file-name "my-init.el" user-emacs-directory))
   (let ((private-file (expand-file-name "private.el" user-emacs-directory)))
     (when (file-exists-p private-file)
       (condition-case nil
           (load private-file)
         (error (message "Error during loading of private settings"))))))
-(setq debug-on-error nil)
 
 (provide 'init)
 ;;; init.el ends here
