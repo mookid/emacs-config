ab32f314ebf5a1f37611dd1d7b65dca3566af81d
mookid <nathan.moreau@m4x.org>
2016-11-08 23:15:18 +0100

    Remove org-mode latex stuff (use pandoc instead.)

--- my-init.el
+++ my-init.el
@@ -931,7 +931,7 @@ Use in `isearch-mode-end-hook'."
     (set-face-attribute tuareg-font-lock-module-face nil
                         :weight 'bold)
     (use-package ocp-indent
-      :demand t
+��Ȫ��ȿ�΢�׺�      :demand t
       :bind (:map tuareg-mode-map ("C-=" . ocp-indent-buffer)))))
 
 ;; C configuration
@@ -958,64 +958,6 @@ Use in `isearch-mode-end-hook'."
 (use-package yasnippet
   :defer t
   :config (yas-reload-all))
-
-(use-package org
-  :defer t
-  :config
-  (progn
-    ;; allow for export=>beamer by placing
-    ;; #+LaTeX_CLASS: beamer in org files
-    (defvar org-export-latex-classes)
-    (unless (boundp 'org-export-latex-classes)
-      (setq org-export-latex-classes nil))
-    (add-to-list 'org-export-latex-classes
-                 ;; beamer class, for presentations
-                 '("beamer"
-                   "\\documentclass[11pt]{beamer}\n
-      \\mode<{{{beamermode}}}>\n
-      \\usetheme{{{{beamertheme}}}}\n
-      \\usecolortheme{{{{beamercolortheme}}}}\n
-      \\beamertemplateballitem\n
-      \\setbeameroption{show notes}
-      \\usepackage[utf8]{inputenc}\n
-      \\usepackage[T1]{fontenc}\n
-      \\usepackage{hyperref}\n
-      \\usepackage{color}
-      \\usepackage{listings}
-      \\lstset{numbers=none,language=[ISO]C++,tabsize=4,
-  frame=single,
-  basicstyle=\\small,
-  showspaces=false,showstringspaces=false,
-  showtabs=false,
-  keywordstyle=\\color{blue}\\bfseries,
-  commentstyle=\\color{red},
-  }\n
-      \\usepackage{verbatim}\n
-      \\institute{{{{beamerinstitute}}}}\n
-       \\subject{{{{beamersubject}}}}\n"
-
-                   ("\\section{%s}" . "\\section*{%s}")
-
-                   ("\\begin{frame}[fragile]\\frametitle{%s}"
-                    "\\end{frame}"
-                    "\\begin{frame}[fragile]\\frametitle{%s}"
-                    "\\end{frame}")))
-
-    ;; letter class, for formal letters
-
-    (add-to-list 'org-export-latex-classes
-
-                 '("letter"
-                   "\\documentclass[11pt]{letter}\n
-      \\usepackage[utf8]{inputenc}\n
-      \\usepackage[T1]{fontenc}\n
-      \\usepackage{color}"
-
-                   ("\\section{%s}" . "\\section*{%s}")
-                   ("\\subsection{%s}" . "\\subsection*{%s}")
-                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                   ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))))
 
 (use-package key-chord
   :init
