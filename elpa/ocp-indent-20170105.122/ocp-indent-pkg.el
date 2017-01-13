8cb32501517ae1ffbc05dbd221cad1bce8e78c0e
mookid <nathan.moreau@m4x.org>
2017-01-13 23:51:29 +0100

    Update/fix.

---
+++ elpa/ocp-indent-20170105.122/ocp-indent-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ocp-indent" "20170105.122" "automatic indentation with ocp-indent" 'nil :url "http://www.typerex.org/ocp-indent.html" :keywords '("ocaml" "languages"))
