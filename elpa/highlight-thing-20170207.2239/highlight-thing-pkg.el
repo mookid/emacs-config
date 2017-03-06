ba292bdfef356e873a3e04d15f44f6b24d900e4b
mookid <nathan.moreau@m4x.org>
2017-03-07 00:53:04 +0100

    Package update.

---
+++ elpa/highlight-thing-20170207.2239/highlight-thing-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight-thing" "20170207.2239" "Minimalistic minor mode to highlight current thing under point." 'nil :url "https://github.com/fgeller/highlight-thing.el" :keywords '("highlight" "thing" "symbol"))
