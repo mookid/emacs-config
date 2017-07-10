43651108d8bba977a873b538c38a44e5d1569688
nathan moreau <nathan.moreau@m4x.org>
2017-07-10 10:50:10 +0200

    Accept changes.

--- .mc-lists.el
+++ .mc-lists.el
@@ -16,6 +16,7 @@
         dabbrev-expand
         delete-indentation
         down-list
+        electric-newline-and-maybe-indent
         electric-pair-delete-pair
         erlang-electric-comma
         forward-sexp
