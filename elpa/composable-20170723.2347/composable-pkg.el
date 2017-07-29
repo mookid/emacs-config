cdfc51b61fe071c8360fd49cf7646425d933d123
nathan moreau <nathan.moreau@m4x.org>
2017-07-29 11:49:15 +0200

    Package update.

---
+++ elpa/composable-20170723.2347/composable-pkg.el
@@ -0,0 +1,7 @@
+(define-package "composable" "20170723.2347" "composable editing"
+  '((emacs "24.4"))
+  :keywords
+  '("lisp"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/merlin-20170630.146/merlin-pkg.el
@@ -0,0 +1,4 @@
+(define-package "merlin" "20170630.146" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
