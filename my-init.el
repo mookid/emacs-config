986c1fb2cb19e25dbe9c8df696971459388a83c8
nathan moreau <nathan.moreau@m4x.org>
2017-08-10 01:27:19 +0200

    Reduce the default height to fit laptops.

--- my-init.el
+++ my-init.el
@@ -471,7 +471,7 @@ FILENAME is a name of a file or a directory."
     (setq show-paren-delay 0)
     (show-paren-mode 1)))
 
-(add-to-list 'default-frame-alist '(height . 37))
+(add-to-list 'default-frame-alist '(height . 30))
 (add-to-list 'default-frame-alist '(width . 80))
 (add-to-list 'initial-frame-alist '(top . 20))
 (add-to-list 'initial-frame-alist '(left . 120))
