b4271235d5f17bfb778a44aa0294e298d6a36205
nathan moreau <nathan.moreau@m4x.org>
2017-08-09 03:58:08 +0200

    Improve build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+set -e
+
 if [ $# -ne 1 ]; then echo "usage: $0 <path-to-emacs-repository>"; exit 2; fi
 
 cd $1
-./autogen.sh
-./configure --with-x-toolkit=lucid --without-pop CFLAGS='-O2'
-make -j 4
+
+echo running autogen.sh...
+./autogen.sh > autogen.out 2> autogen.err
+
+echo running configure script...
+./configure \
+    --without-makeinfo\
+    --with-x-toolkit=lucid\
+    --without-pop\
+    CFLAGS='-O2'\
+    > configure.out 2> configure.err
+
+echo running make...
+make -j 4 > build.out 2> build.err
+
+echo ok
