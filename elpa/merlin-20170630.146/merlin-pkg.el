7d4d61954e5f838572120f22bda4768a3025bd0c
nathan moreau <nathan.moreau@m4x.org>
2017-10-13 20:10:14 +0200

    Package update.

--- elpa/merlin-20170630.146/merlin-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "merlin" "20170630.146" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/ocp-indent-20170412.12/ocp-indent-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ocp-indent" "20170412.12" "automatic indentation with ocp-indent" 'nil :url "http://www.typerex.org/ocp-indent.html" :keywords '("ocaml" "languages"))
