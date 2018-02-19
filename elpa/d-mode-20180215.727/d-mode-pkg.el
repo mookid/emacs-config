a40c23435ccfd0edf8a3b96bea74989170d875fd
nathan moreau <nathan.moreau@m4x.org>
2018-02-19 14:29:58 +0100

    Package update.

---
+++ elpa/d-mode-20180215.727/d-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "d-mode" "20180215.727" "D Programming Language major mode for (X)Emacs" 'nil :commit "398f1854d53863185e60873b494eec0263bdd0dd" :keywords '("d" "programming" "language" "emacs" "cc-mode"))
