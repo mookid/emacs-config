feb702107c4ddd224d585ce5b60598b1b25a3cd0
nathan moreau <nathan.moreau@m4x.org>
2018-04-08 22:06:21 +0200

    Package update.

---
+++ elpa/package-utils-20180317.1134/package-utils-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "package-utils" "20180317.1134" "Extensions for package.el" '((restart-emacs "0.1.1")) :commit "652531caf1a1df916d2185e7f47085bd0509c940" :keywords '("package" "convenience") :url "https://github.com/Silex/package-utils")
