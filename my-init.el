e29d657d78f04c5054b0392f6af8e444ce0ea006
nathan moreau <nathan.moreau@m4x.org>
2018-05-24 21:04:05 +0200

    Put back emacs-lisp-mode as initial-major-mode.

--- my-init.el
+++ my-init.el
@@ -19,7 +19,6 @@
 (setq set-mark-command-repeat-pop t)
 (setq completion-cycle-threshold 5)
 (setq initial-scratch-message nil)
-(setq initial-major-mode 'fundamental-mode)
 (setq mouse-wheel-progressive-speed nil)
 (setq next-line-add-newlines nil)
 (setq require-final-newline t)
@@ -890,6 +889,7 @@ if its size is 1 line."
 (use-package ielm
   :init
   (progn
+    (setq initial-major-mode 'emacs-lisp-mode)
     (setq eval-expression-print-level nil)
     (use-package elisp-mode
       :bind
