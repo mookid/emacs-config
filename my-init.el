964d7f697b72b63e53c9fd7e00d13c1860409422
Nathan Moreau <nathan.moreau@m4x.org>
2019-04-09 09:56:50 +0200

    Set ivy-height back to default.

--- my-init.el
+++ my-init.el
@@ -1327,10 +1327,9 @@ In that case, insert the number."
   (setq ivy-count-format "(%d/%d) ")
   (use-package ivy-hydra)
   :config
-  (progn
-    (setq ivy-height 30)
-    (setq ivy-virtual-abbreviate 'full)
-    (my-key-chord-define ivy-minibuffer-map "fh" 'ivy-avy)))
+  (setq ivy-height 10)
+  (setq ivy-virtual-abbreviate 'full)
+  (my-key-chord-define ivy-minibuffer-map "fh" 'ivy-avy))
 
 (use-package counsel
   :preface
